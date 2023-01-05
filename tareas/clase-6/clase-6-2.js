15007214e40f93c2a08fc0466b23154a5c658044
luciima <luciiamar19@gmail.com>
2023-01-05 01:25:44 -0300

    Agrega funciones auxiliares
    Estas funciones calculan lo que se le mostrará al usuario (salario mensual, mayor, menor y promedio de salarios de la familia)

--- tareas/clase-6/clase-6-2.js
+++ tareas/clase-6/clase-6-2.js
@@ -7,3 +7,36 @@ menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
+
+function calcularSalarioMensual(salarioAnual) {
+    const MESES_ANIO = 12;
+    return salarioAnual / MESES_ANIO;
+}
+
+function hallarMayorNumero(numeros) {
+    let mayorNumero = Number(numeros[0]);
+    for (let numero of numeros) {
+        if (numero > mayorNumero) {
+            mayorNumero = numero;
+        }
+    }
+    return mayorNumero;
+}
+
+function hallarMenorNumero(numeros) {
+    let menorNumero = Number(numeros[0]);
+    for (let numero of numeros) {
+        if (numero < numeros) {
+            menorNumero = numero;
+        }
+    }
+    return menorNumero;
+}
+
+function hallarPromedio(numeros) {
+    let suma = 0;
+    for (let numero of numeros) {
+        suma += Number(numero);
+    }
+    return suma / numeros.length;
+}
