455c1e38b138a2cca88bb96c8376ebc98074c06d
luciima <luciiamar19@gmail.com>
2023-02-01 15:15:30 -0300

    Mueve las funciones de cálculos a otro archivo js

--- tareas/clase-6/tarea-2/clase-6-2.js
+++ tareas/clase-6/tarea-2/clase-6-2.js
@@ -8,43 +8,6 @@ menor salario anual, salario anual promedio y salario mensual promedio.
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
 
-function calcularSalariosMensuales(salariosAnuales) {
-    const MESES_ANIO = 12;
-    let salariosMensuales = [];
-    for (let salarioAnual of salariosAnuales) {
-        salariosMensuales.push(salarioAnual / MESES_ANIO);
-    }
-    return salariosMensuales;
-}
-
-function hallarMayorNumero(numeros) {
-    let mayorNumero = Number(numeros[0]);
-    for (let numero of numeros) {
-        if (numero > mayorNumero) {
-            mayorNumero = numero;
-        }
-    }
-    return mayorNumero;
-}
-
-function hallarMenorNumero(numeros) {
-    let menorNumero = Number(numeros[0]);
-    for (let numero of numeros) {
-        if (numero < menorNumero) {
-            menorNumero = numero;
-        }
-    }
-    return menorNumero;
-}
-
-function hallarPromedio(numeros) {
-    let suma = 0;
-    for (let numero of numeros) {
-        suma += Number(numero);
-    }
-    return suma / numeros.length;
-}
-
 function crearIntegrante($formulario, numeroIntegrante) {
     const etiqueta = document.createElement("label");
     etiqueta.setAttribute("for", `integrante-${numeroIntegrante}`);
